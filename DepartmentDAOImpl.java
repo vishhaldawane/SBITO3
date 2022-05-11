ef126eafe21a3a76b404bc406c50388babf35132
vishhaldawane <45403067+vishhaldawane@users.noreply.github.com>
2022-05-11 14:55:36 +0530

    SALES

--- DepartmentDAOImpl.java
+++ DepartmentDAOImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
@@ -18,6 +19,19 @@ public class DepartmentDAOImpl implements DepartmentDAO {
 		EntityManagerFactory factory = Persistence.createEntityManagerFactory("MyJPA");
 		entityManager = factory.createEntityManager();
 	}
+	
+	public Department findDepartmentById(int deptno)
+	{
+		return entityManager.find(Department.class,deptno);
+	}
+	public void updateDepartment(Department dept)
+	{
+		EntityTransaction et= entityManager.getTransaction();
+		et.begin();
+			entityManager.merge(dept);
+		et.commit();
+	}
+	
 	@Override
 	public List<Department> findAllDepartments() {
 		TypedQuery<Department> query = 
--- TestOneToMany.java
+++ TestOneToMany.java
@@ -3,10 +3,15 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 public class TestOneToMany {
 	DepartmentDAO deptDAO = new DepartmentDAOImpl();
+	
 	@Test
 	public void loadAllDepartmentsTest() {
+		
 		List<Department> deptList = deptDAO.findAllDepartments();
-		for (Department department : deptList) {
+		
+		for (Department department : deptList) 
+		{
+			
 			System.out.println(department.getDepartmentNumber()+
 					" "+department.getDepartmentName()+
 					" "+department.getDepartmentLocation());
@@ -25,4 +30,33 @@ public class TestOneToMany {
 			}
 		} 
 	}
+	
+	@Test
+	public void updateSalaryOfSalesmanTest() 
+	{
+		Department department = deptDAO.findDepartmentById(30);
+			System.out.println(department.getDepartmentNumber()+
+			" "+department.getDepartmentName()+
+			" "+department.getDepartmentLocation());
+			System.out.println("=======================================");
+			
+			List<Employee> empList = department.getEmployeeList();
+			for (Employee emp : empList) 
+			{
+				if(emp.getJob().equals("SALESMAN")) 
+				{
+					System.out.println("EMP number : "+emp.getEmployeeNumber());
+					System.out.println("EMP name   : "+emp.getEmployeeName());
+					System.out.println("EMP job    : "+emp.getJob());
+					System.out.println("EMP mgr    : "+emp.getEmployeeManagerCode());
+					System.out.println("EMP doj    : "+emp.getJoiningDate());
+					System.out.println("EMP salary : "+emp.getBasicSalary());
+					System.out.println("EMP comm   : "+emp.getCommission());
+					System.out.println("EMP dept no: "+emp.getDepartment().getDepartmentNumber());
+					System.out.println("------------------------");
+					emp.setBasicSalary(emp.getBasicSalary()+emp.getBasicSalary()*0.10);
+				}//end of if for salesman
+			}//end of inner for - emp table
+			deptDAO.updateDepartment(department);
+	} //end of test case
 }
